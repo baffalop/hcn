fea5696ea87138b58d35799b5e5b4c618f91adad
Nikita Gaidakov <ngaidakov@podfather.com>
2022-06-16 22:50:02 +0100

    Add button click transition

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -8,8 +8,8 @@ export let id: string
   {id}
   on:click={() => $count += 1}
   data-testid={id}
-  class="rounded-full py-4 px-8 border-2 border-transparent outline-none w-48 tabular-nums
-    bg-svelte/10 active:bg-svelte/20 focus:border-opacity-100"
+  class="rounded-full py-4 px-8 border-2 border-transparent outline-none w-48 tabular-nums focus:border-opacity-100
+    bg-svelte/10 active:bg-svelte/20 transition-colors duration-200 ease-in active:transition-none"
 >
   Clicks: {$count}
 </button>

3215bde296a0baa2151c3cd2fa2ceca4203b2332
Nikita Gaidakov <ngaidakov@podfather.com>
2022-07-27 00:52:33 -0400

    Add HCN primary colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: ['Inter', 'Avenir', 'Helvetica', '-apple-system', 'BlinkMacSystemFont', 'sans-serif']
+      fontFamily: ['Inter', 'Avenir', 'Helvetica', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+      colors: {
+        primary: {
+          orange: '#faa732',
+          green: '#50c3a4',
+          purple: '#af87c3',
+          blue: '#5a9fdc',
+          pink: '#e65583',
+        },
+      },
     },
   },
   plugins: [],

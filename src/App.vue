97cf427877cec6de7c1bdd4888d249602c495af6
Nikita Gaidakov <ngaidakov@podfather.com>
2022-08-23 14:57:33 +0100

    Add Vue router and scaffold single route

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-import Media from '@components/Media.vue'
-import Timeline from '@components/Timeline.vue'
-
-const playing = ref(false)
-const duration = ref(1)
-const time = ref(0)
-</script>
-
 <template>
-  <div class="flex items-center gap-4 w-3/4 max-w-lg">
-    <button class="text-lg cursor-pointer w-6" @click="playing = !playing">
-      <template v-if="playing">❚❚</template>
-      <template v-else>▶</template>
-    </button>
-
-    <Timeline
-      v-model:time="time"
-      :resolution="34"
-      :duration="duration"
-      :playing="playing"
-      class="flex-grow"
-    />
-  </div>
-
-  <Media
-    type="video"
-    src="/with-audio.mp4"
-    preload="auto"
-    playsinline
-    class="fixed inset-0 -z-10 w-screen h-screen object-cover"
-    v-model:time="time"
-    v-model:playing="playing"
-    @update:duration="duration = $event"
-  />
+  <router-view />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
 import './tailwind.css'
 import './styles.css'
-import App from './App.vue'
+import App from '@/App.vue'
+import Player from '@components/Player.vue'
 
-createApp(App).mount('#app')
+const routes: RouteRecordRaw[] = [
+  { path: '/', component: Player }
+]
+
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+app.use(router)
+
+app.mount('#app')

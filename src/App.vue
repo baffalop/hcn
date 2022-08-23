1ed97cf630d268b37290e1e2a8d415e5328c8822
Nikita Gaidakov <ngaidakov@podfather.com>
2022-08-23 22:34:21 +0100

    Add home link

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const isOnIndexPage = computed(() => route.name === 'index')
+</script>
+
 <template>
+  <router-link
+    v-if="!isOnIndexPage"
+    :to="{ name: 'index' }"
+    class="fixed left-20 top-20 text-primary-blue"
+  >Home</router-link>
+
   <router-view />
 </template>
 

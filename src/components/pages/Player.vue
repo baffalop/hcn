0310285aa99bf6d126e88c9b603f510832ea1295
Nikita Gaidakov <ngaidakov@podfather.com>
2022-09-06 22:30:40 +0100

    Add rew/ffw buttons, on another row

--- src/components/pages/Player.vue
+++ src/components/pages/Player.vue
@@ -10,19 +10,28 @@ const time = ref(0)
 </script>
 
 <template>
-  <div class="flex items-center gap-4 w-3/4 max-w-lg">
-    <button class="text-lg cursor-pointer w-6 invert" @click="playing = !playing">
-      <img v-show="!playing" src="/icon/play-simple.svg" alt="Play">
-      <img v-show="playing" src="/icon/pause-simple.svg" alt="Pause">
-    </button>
-
+  <div class="space-y-8 w-3/4 max-w-lg">
     <Timeline
       v-model:time="time"
       :resolution="34"
       :duration="duration"
       :playing="playing"
-      class="flex-grow"
     />
+
+    <div class="flex items-center justify-center gap-6">
+      <button class="control" @click="time -= 10">
+        <img src="/icon/rew-simple.svg" alt="Back 10 seconds">
+      </button>
+
+      <button class="control" @click="playing = !playing">
+        <img v-show="!playing" src="/icon/play-simple.svg" alt="Play">
+        <img v-show="playing" src="/icon/pause-simple.svg" alt="Pause">
+      </button>
+
+      <button class="control" @click="time += 10">
+        <img src="/icon/ffw-simple.svg" alt="Forward 10 seconds">
+      </button>
+    </div>
 
     <Media
       type="video"
@@ -41,5 +50,9 @@ const time = ref(0)
 <style scoped>
 video {
   filter: invert(0.8) brightness(0.4) sepia(1) saturate(1.5) hue-rotate(139deg);
+}
+
+.control {
+  @apply cursor-pointer w-10 invert;
 }
 </style>

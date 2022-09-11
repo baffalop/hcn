b023f142b7db81e5dd2f047a2738f81922fc1e75
Nikita Gaidakov <ngaidakov@podfather.com>
2022-09-11 22:14:44 +0100

    Add lorem ipsum to the index

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="px-4 space-y-3">
+  <div class="px-4 max-w-screen-md space-y-3">
     <p>
       Welcome to some audio guides for Holocaust Centre North.
     </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
     <p>
       Proceed to <RouterLink :to="{ name: 'player' }">player</RouterLink>
     </p>

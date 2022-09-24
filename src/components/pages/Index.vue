90aac81b1858b32a0b837483b9bf9df6053dcff5
Nikita Gaidakov <ngaidakov@podfather.com>
2022-09-24 15:12:10 +0100

    Add tracklist

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+interface Track {
+  title: string
+}
+
+const lindaTracks: Track[] = [
+  { title: 'Constant Vigilance' },
+  { title: 'Restrictions and Ejection' },
+  { title: 'Saying Goodbye' },
+  { title: 'Wires and Fences' },
+]
+
+function formatTrackNumber (i: number): string {
+  return (i + 1).toFixed(0).padStart(2, '0')
+}
+</script>
+
 <template>
-  <div class="px-4 max-w-screen-md space-y-3">
+  <div class="h-full px-4 max-w-screen-md">
     <p>
       Welcome to some audio guides for Holocaust Centre North.
     </p>
@@ -8,9 +25,14 @@
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
 
-    <p>
-      Proceed to <RouterLink :to="{ name: 'player' }">player</RouterLink>
-    </p>
+    <h2 class="mt-10 mb-6 text-2xl">Linda O'Keefe</h2>
+
+    <ul>
+      <li v-for="(track, i) in lindaTracks">
+        {{ formatTrackNumber(i) }}.
+        <RouterLink :to="{ name: 'player' }">{{ track.title }}</RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
 

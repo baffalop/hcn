8065f6bec98ac03397a27bddde0bfea8876fd58f
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-04 17:47:11 +0100

    Generate all shades of primary-green, use for link colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,19 @@ module.exports = {
       colors: {
         primary: {
           orange: '#faa732',
-          green: '#50c3a4',
+          green: {
+            DEFAULT: '#50C3A4',
+            '50': '#D9F2EB',
+            '100': '#CAEDE3',
+            '200': '#ABE2D3',
+            '300': '#8DD8C4',
+            '400': '#6ECDB4',
+            '500': '#50C3A4',
+            '600': '#38A386',
+            '700': '#2A7964',
+            '800': '#1B4F41',
+            '900': '#0D261F',
+          },
           purple: '#af87c3',
           blue: '#5a9fdc',
           pink: '#e65583',

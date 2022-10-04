4c2828f7501b11f14f333d5945b4da729a54029a
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-04 14:54:20 +0100

    Extract track data to JSON file

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,14 +1,11 @@
+import tracksJson from './tracks.json'
+
 export interface Track {
   title: string
   slug: string
 }
 
-export const tracks: Track[] = [
-  { title: 'Constant Vigilance', slug: 'constant-vigilance' },
-  { title: 'Restrictions and Ejection', slug: 'restrictions-and-ejection' },
-  { title: 'Saying Goodbye', slug: 'saying-goodbye' },
-  { title: 'Wire and Fences', slug: 'wire-and-fences' },
-]
+export const tracks = tracksJson as Track[]
 
 export function fromSlug (slug: string): Track|null {
   return tracks.find(track => track.slug === slug) ?? null

f3581cdb0b27e6a6b27b828ace45d025882e3f69
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-08 22:59:51 +0100

    Extend range styling to Firefox (it becomes a nightmare)

--- src/components/player/TimelineSlider.vue
+++ src/components/player/TimelineSlider.vue
@@ -4,7 +4,8 @@
     v-model.number="current"
     :max="max"
     :step="0.1"
-    class="h-2 cursor-pointer bg-transparent appearance-none focus:outline-none focus:shadow-none"
+    class="h-2 cursor-pointer bg-transparent rounded-full overflow-hidden
+      appearance-none focus:outline-none focus:shadow-none"
     :style="{
       '--progress-percent': `${progressPercent}%`,
       '--progress': `${progress}`,
@@ -42,12 +43,11 @@ const progressPercent = computed(() => progress.value * 100)
 @tailwind components;
 
 input::-webkit-slider-runnable-track {
-  @apply h-1 rounded-full duration-200 ease-out;
+  @apply track;
   --thumb-size: 0.25rem;
   /* position of cutoff for fill colour, adjusted for thumb position */
   /* see https://css-tricks.com/value-bubbles-for-range-inputs/#:~:text=We%20can%20use%20some%20magic%20numbers%20there%20that%20seem%20to%20work%20decently%20well%20across%20browsers%3A */
   --fill-position: calc(var(--progress-percent) + (var(--thumb-size) / 2) - (var(--progress) * var(--thumb-size)));
-  transition-property: height;
   background: linear-gradient(
     to right,
     theme('colors.gray.100') 0%,
@@ -63,20 +63,63 @@ input:hover::-webkit-slider-runnable-track {
 }
 
 input::-webkit-slider-thumb {
-  @apply thumb;
+  @apply thumb h-full;
 }
 
 input::-moz-range-thumb {
-  @apply thumb;
+  @apply thumb h-1 rounded-l-none;
 }
 
-input::-ms-thumb {
-  @apply thumb;
+input:hover::-moz-range-thumb {
+  @apply h-2;
+}
+
+input::-moz-range-track {
+  @apply track bg-gray-600;
+}
+
+input:hover::-moz-range-track {
+  @apply h-2;
+}
+
+input::-moz-range-progress {
+  @apply h-1 rounded-l-full bg-gray-100 h-transition -mr-1;
+}
+
+input:hover::-moz-range-progress {
+  @apply h-2;
+}
+
+input::-ms-thumb { @apply thumb }
+
+input::-ms-track {
+  @apply track bg-gray-600;
+}
+
+input:hover::-ms-track {
+  @apply h-2;
+}
+
+input::-ms-fill-lower {
+  @apply bg-gray-100;
+}
+
+input::-ms-fill-upper {
+  @apply bg-gray-600;
 }
 
 @layer components {
+  .h-transition {
+    transition-property: height;
+    @apply duration-200 ease-out;
+  }
+
   .thumb {
-    @apply appearance-none h-full w-3 rounded-full bg-gray-100;
+    @apply appearance-none border-none w-2 rounded-full bg-gray-100 h-transition;
+  }
+
+  .track {
+    @apply h-1 rounded-full h-transition;
   }
 }
 </style>

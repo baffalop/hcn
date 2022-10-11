6ddaad70e8e2250e2a1ae678d04cdbcb79116221
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-11 16:13:42 +0100

    Remove unnecessary nullish chaining

--- src/components/pages/Player.vue
+++ src/components/pages/Player.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full flex flex-col justify-center mx-auto w-4/5 max-w-lg">
-    <h2 class="text-4xl mb-14">{{ track?.title }}</h2>
+    <h2 class="text-4xl mb-14">{{ track.title }}</h2>
 
     <Timeline v-model:time="time" :duration="duration" :playing="playing" />
 

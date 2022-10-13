d12c3c46f635dbf7071a4b10affd3ccee3472f27
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-13 01:41:23 +0100

    More transcription

--- src/components/pages/Transcript.vue
+++ src/components/pages/Transcript.vue
@@ -56,7 +56,10 @@ interface Line {
 }
 
 const transcription = ref<Line[]>([
-  {"start":109.756262,"line":""},
+  {"start":148.445383,"line":"And not be part of the crowd","end":150.057817},
+  {"start":141.622436,"line":"And, it's very very difficult for a kid of that age to resist that temptation","end":148.445383},
+  {"start":137.913398,"line":"Things like that as they had in the Hitler Youth","end":141.622436},
+  {"start":134.186143,"line":"Uniforms, gunfires, learning to shoot rifles","end":137.913398},
   {"start":105.05566,"line":"to resist the temptations the Nazis offered for kids of that age","end":109.756262},
   {"start":101.749868,"line":"You know, it's very difficult for a 12, 13, 14 year-old","end":105.05566},
   {"start":81.545969,"line":"And I probably would","end":83.677251},
@@ -65,7 +68,7 @@ const transcription = ref<Line[]>([
   {"start":70.002884,"line":"but I'm concerned that...","end":73.704323},
   {"start":60.522463,"line":"Well obviously my contemporaries who committed these atrocities are dreadful","end":70.002884},
   {"start":54.747394,"line":"Well, you know you're a survivor because you're there","end":60.522463},
-  {"start":53.908808,"line":"And, erm...","end":54.747394},
+  {"start":53.908808,"line":"And, erm...","end":54.747394}
 ])
 
 const currentLine = computed(() => findMostRecentLine(transcription.value, time.value))

706ed6bba1e9278a0f153fc7291d9e45c481a8fc
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-29 00:14:52 +0100

    Start parsing srt file: validate extension, get text from file

--- src/components/player/DroppableTranscript.vue
+++ src/components/player/DroppableTranscript.vue
@@ -7,14 +7,34 @@
 <script setup lang="ts">
 import FileDrop from '@components/FileDrop.vue'
 import Transcript, { Transcription } from '@components/player/Transcript.vue'
+import { ref } from 'vue'
 
 const props = defineProps<{
   transcription: Transcription
   time: number
 }>()
 
-function onDrop (file: File): void {
-  console.log('drop', file)
+const droppedSrt = ref<string|null>()
+
+async function onDrop (file: File): Promise<void> {
+  try {
+    await parseFile(file)
+  } catch (e) {
+    alert('Wrong file type: please drop a .srt file')
+  }
+}
+
+async function parseFile (file: File): Promise<void> {
+  if (!looksLikeSrt(file)) {
+    throw new Error('no .srt extension')
+  }
+
+  droppedSrt.value = await file.text()
+  console.log('srt', droppedSrt.value)
+}
+
+function looksLikeSrt (file: File): boolean {
+  return /\.srt$/.test(file.name)
 }
 
 </script>

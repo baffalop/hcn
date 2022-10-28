9a46c9e8bebfde71b8b4f956a83cf75c666f53a7
Nikita Gaidakov <ngaidakov@podfather.com>
2022-10-29 00:31:20 +0100

    Parse dropped srt and pass to Transcript component

--- src/components/player/DroppableTranscript.vue
+++ src/components/player/DroppableTranscript.vue
@@ -1,36 +1,49 @@
 <template>
   <FileDrop @drop="onDrop">
-    <Transcript :transcription="transcription" :time="time" />
+    <Transcript :transcription="droppedTranscription ?? transcription" :time="time" />
   </FileDrop>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import SrtParser from 'srt-parser-2'
+
 import FileDrop from '@components/FileDrop.vue'
 import Transcript, { Transcription } from '@components/player/Transcript.vue'
-import { ref } from 'vue'
 
 const props = defineProps<{
   transcription: Transcription
   time: number
 }>()
 
-const droppedSrt = ref<string|null>()
+const parser = ref<SrtParser|null>(null)
+const droppedTranscription = ref<Transcription|null>()
 
 async function onDrop (file: File): Promise<void> {
   try {
-    await parseFile(file)
+    droppedTranscription.value = await parseFile(file)
   } catch (e) {
     alert('Wrong file type: please drop a .srt file')
   }
 }
 
-async function parseFile (file: File): Promise<void> {
+async function parseFile (file: File): Promise<Transcription> {
   if (!looksLikeSrt(file)) {
     throw new Error('no .srt extension')
   }
 
-  droppedSrt.value = await file.text()
-  console.log('srt', droppedSrt.value)
+  const contents = await file.text()
+
+  if (parser.value === null) {
+    parser.value = new SrtParser()
+  }
+
+  const parsed = parser.value.fromSrt(contents)
+  return parsed.map(line => ({
+    ...line,
+    start: line.startSeconds,
+    end: line.endSeconds,
+  }))
 }
 
 function looksLikeSrt (file: File): boolean {

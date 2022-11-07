5951d60c07d18630c358a0f9ecd2cf1d69c22503
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-07 15:02:49 +0000

    Adjust new icon dimensions: allow overriding viewBox on Icon component

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,13 +1,16 @@
 <template>
-  <svg viewBox="0 0 350 350">
+  <svg :viewBox="viewBox">
     <use :href="`${src}#icon`"></use>
   </svg>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   src: string
-}>()
+  viewBox?: `${number} ${number} ${number} ${number}`
+}>(), {
+  viewBox: '0 0 350 350'
+})
 </script>
 
 <style scoped>

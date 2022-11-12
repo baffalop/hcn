fcb6fb590fcbaacdda958b6e5d73697df852d0b1
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-12 15:17:01 +0000

    Reinstate keying route component by slug

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <RouterView v-slot="{ Component }">
     <PageTransition>
-      <component :is="Component" />
+      <component :key="route.params?.slug ?? route.name" :is="Component" />
     </PageTransition>
   </RouterView>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+
 import PageTransition from '@components/PageTransition.vue'
+
+const route = useRoute()
 </script>
 
 <style scoped>

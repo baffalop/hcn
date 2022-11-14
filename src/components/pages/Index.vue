26fbce0757ecd8789cd81a99e469bd5fb2e0ed29
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-14 15:27:39 +0000

    Set body bg to stone, index bg to brick

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full w-full">
+  <div class="bg-primary-brick h-full w-full">
     <div class="h-4/5 px-4 mx-auto max-w-screen-md flex flex-col justify-center">
       <p>
         Welcome to some audio guides for Holocaust Centre North.
@@ -14,7 +14,7 @@
       </p>
     </div>
 
-    <div id="menu" class="h-screen w-full px-3 mx-auto flex flex-col justify-center">
+    <div id="menu" class="bg-primary-brick h-screen w-full px-3 mx-auto flex flex-col justify-center">
       <h2 class="mt-10 mb-6 text-3xl">Menu</h2>
 
       <table class="text-left w-auto mx-auto font-editorial">
@@ -37,12 +37,4 @@ import { formatSecs } from '@/utils/time'
 </script>
 
 <style scoped>
-#menu {
-  background: linear-gradient(
-    to bottom,
-    transparent,
-    theme(colors.primary.green.900) 20%,
-    theme(colors.primary.green.900)
-  );
-}
 </style>

6212e9301e63e61b37551f572dbea3585b1453c4
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-16 00:21:46 +0000

    Remove unused colour setting from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <RouterView v-slot="{ Component }">
     <PageTransition>
-      <component :is="Component" :key="route.params?.slug ?? route.name" @update:color="color = $event" />
+      <component :is="Component" :key="route.params?.slug ?? route.name" />
     </PageTransition>
   </RouterView>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 import PageTransition from '@components/PageTransition.vue'
 
 const route = useRoute()
-
-const baseColor = 'theme(color.stone.900)'
-const color = ref<string>(baseColor)
-
-function setBackground (color: string): void {
-  document.body.style.backgroundColor = color
-}
 </script>
 
 <style scoped>

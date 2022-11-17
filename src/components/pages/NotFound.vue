89267ad00f07e065a9f4802289e01723da12fe99
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-17 23:20:43 +0000

    Apply filter-accent to 404 title

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-full flex flex-col gap-4 items-center justify-center">
-    <h1 class="text-7xl">404</h1>
+    <h1 class="text-7xl text-stone-600 filter-accent">404</h1>
     <h4 class="text-2xl">Page not found</h4>
   </div>
 </template>

ea3bd31a2657f76631a095b733e339a2c212d62c
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-17 23:02:14 +0000

    Redesign 404 page

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="space-y-4">
+  <div class="h-full flex flex-col gap-4 items-center justify-center">
     <h1 class="text-7xl">404</h1>
-    <h4 class="text-2xl">Not found</h4>
+    <h4 class="text-2xl">Page not found</h4>
   </div>
 </template>
+
+<script setup lang="ts">
+import { setBackground } from '@/composable/body'
+
+setBackground({ class: 'bg-stone-600' })
+</script>
 
 <style scoped>
 </style>

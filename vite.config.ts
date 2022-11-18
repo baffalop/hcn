4ebc8799f6ef0ab4c97fe0357cf4fa2d1d21c5ba
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-18 01:17:18 +0000

    Merge PWA generation
    Squashed commit of the following:
    
    commit a5c1e5f54fb5bc4961523ca6d3fa9064fcac9f61
    Author: Nikita Gaidakov <ngaidakov@podfather.com>
    Date:   Fri Nov 18 01:14:46 2022 +0000
    
        Add robots.txt
    
    commit c055463fed6d166b00cfe22d7da39b5cca9a9365
    Author: Nikita Gaidakov <ngaidakov@podfather.com>
    Date:   Fri Nov 18 01:14:14 2022 +0000
    
        Add png and jpeg to workbox glob
    
    commit 5cda7abf6b5239852ece6deab00091ff07035564
    Author: Nikita Gaidakov <ngaidakov@podfather.com>
    Date:   Fri Nov 18 01:13:41 2022 +0000
    
        Add manifest config
    
    commit 87c9b6d4ef2d67e23ede5639240a5d40fd04e434
    Author: Nikita Gaidakov <ngaidakov@podfather.com>
    Date:   Sun Nov 13 23:07:50 2022 +0000
    
        Add precache globPatterns for basic files (svg, otf)
    
    commit fc01b5347add9f3e20555b1710e0b9f26110bfcd
    Author: Nikita Gaidakov <ngaidakov@podfather.com>
    Date:   Sun Nov 13 22:58:57 2022 +0000
    
        Add pwa plugin to vite config, with minimal options
    
    commit 0d66a655c9856658702cf5192ab44060c937bd4b
    Author: Nikita Gaidakov <ngaidakov@podfather.com>
    Date:   Sun Nov 13 22:33:55 2022 +0000
    
        Install vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import checker from 'vite-plugin-checker'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,38 @@ export default defineConfig({
   plugins: [
     vue(),
     checker({ vueTsc: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,svg,otf,png,jpeg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#6c6966',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {

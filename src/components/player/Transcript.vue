f15a265800e5f7cd6c52b4baf9ada56a20304b63
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-19 16:06:34 +0000

    Rewrite transcript line finding logic

--- src/components/player/Transcript.vue
+++ src/components/player/Transcript.vue
@@ -12,19 +12,19 @@
     leave-active-class="!duration-500 ease-in-out absolute w-screen transform"
   >
     <p
-      :key="enabled ? previousLine.start.toFixed(0) : 0"
+      :key="enabled ? previousLine?.start.toFixed(0) ?? 0 : 0"
       class="text-gray-100/60 transition-opacity duration-700"
       :class="enabled && hasPreviousLine ? 'opacity-100' : 'opacity-0'"
     >
-      {{ enabled ? previousLine.text : '' }}
+      {{ enabled ? previousLine?.text : '' }}
     </p>
 
     <p
-      :key="enabled ? currentLine.start.toFixed(0) : 0"
+      :key="enabled ? currentLine?.start.toFixed(0) ?? 0 : 0"
       class="text-grey-100 transition-opacity duration-700"
       :class="enabled && hasCurrentLine ? 'opacity-100' : 'opacity-0'"
     >
-      {{ enabled ? currentLine.text : '' }}
+      {{ enabled ? currentLine?.text : '' }}
     </p>
   </TransitionGroup>
 </template>
@@ -38,6 +38,8 @@ export interface TranscriptionLine {
   end: number
   text: string
 }
+
+type LineEntry = [index: number, line: TranscriptionLine] | [null, null]
 
 const props = withDefaults(defineProps<{
   enabled?: boolean
@@ -54,14 +56,15 @@ const emit = defineEmits<{
 
 const EXPIRY_SECS = 4
 
-const currentLine = computed(() => findMostRecentLine(props.transcript, props.time))
-const previousLine = computed(() => findMostRecentLine(
-  props.transcript.filter(line => line.text !== currentLine.value.text),
-  props.time
-))
+const currentEntry = computed<LineEntry>(() => findMostRecentLine(props.transcript, props.time))
+const currentIndex = computed<number|null>(() => currentEntry.value[0])
+const currentLine = computed<TranscriptionLine|null>(() => currentEntry.value[1])
+const previousLine = computed<TranscriptionLine|null>(
+  () => currentIndex.value ? props.transcript[currentIndex.value - 1] ?? null : null
+)
 
-const hasCurrentLine = computed(() => isExtant(currentLine.value))
-const hasPreviousLine = computed(() => isExtant(previousLine.value))
+const hasCurrentLine = computed(() => isExtant(currentLine.value, props.time))
+const hasPreviousLine = computed(() => isExtant(previousLine.value, props.time))
 
 watch(() => props.enabled, enabled => {
   if (!hasCurrentLine.value) {
@@ -75,19 +78,22 @@ watch(() => hasCurrentLine.value, has => {
   }
 })
 
-function findMostRecentLine (lines: TranscriptionLine[], time: number): TranscriptionLine {
-  return lines.reduce((prev, cur) => {
-    if (cur.start > time) return prev
-    return cur.start > prev.start ? cur : prev
-  }, blankLine())
+function findMostRecentLine (lines: TranscriptionLine[], time: number, to?: number): LineEntry {
+  let prev: LineEntry = [null, null]
+
+  for (const [index, line] of lines.slice(0, to).entries()) {
+    if (line.start > time) {
+      return prev
+    }
+
+    prev = [index, line]
+  }
+
+  return [null, null]
 }
 
-function isExtant (line: TranscriptionLine): boolean {
-  return !!line.end && props.time - line.end < EXPIRY_SECS
-}
-
-function blankLine (): TranscriptionLine {
-  return { start: -1, end: 0, text: '' }
+function isExtant (line: TranscriptionLine|null, time: number): boolean {
+  return line !== null && line.end + EXPIRY_SECS >= time
 }
 </script>
 

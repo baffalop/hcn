c9c7678fe6d63af41fd804b2864f494d9f695481
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-23 18:35:39 +0000

    Change any-maskable icon to app-icon (with no drop shadow)

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
             type: 'image/png',
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'app-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',

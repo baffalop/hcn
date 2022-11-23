20e9a15830464a0d330bbe8fd5e0dfbd1947fffe
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-23 22:53:38 +0000

    Cleanup outdated caches

--- src/sw.ts
+++ src/sw.ts
@@ -1,10 +1,12 @@
-import { precacheAndRoute, PrecacheEntry } from 'workbox-precaching'
+import { cleanupOutdatedCaches, precacheAndRoute, PrecacheEntry } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 declare const self: ServiceWorkerGlobalScope
   & typeof globalThis
   & { __WB_MANIFEST: PrecacheEntry[] }
+
+cleanupOutdatedCaches()
 
 precacheAndRoute(self.__WB_MANIFEST)
 

9351de7873b869e5f49e85a50b6bd37778776ad3
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-23 22:42:51 +0000

    Add Workbox strategies: stale-while-revalidate for scripts, cache-first for media

--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,23 @@
 import { precacheAndRoute, PrecacheEntry } from 'workbox-precaching'
+import { registerRoute } from 'workbox-routing'
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 declare const self: ServiceWorkerGlobalScope
   & typeof globalThis
   & { __WB_MANIFEST: PrecacheEntry[] }
 
 precacheAndRoute(self.__WB_MANIFEST)
+
+registerRoute(
+  ({ request, sameOrigin }) => sameOrigin && ['document', 'script', 'style'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'script',
+  })
+)
+
+registerRoute(
+  ({ request, sameOrigin }) => sameOrigin && ['audio', 'video'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'media',
+  })
+)

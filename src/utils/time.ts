52a91a9e820167e142873cb1b65bd7115e774084
Nikita Gaidakov <ngaidakov@podfather.com>
2022-11-26 15:22:22 +0000

    Ensure we don't see timestamps of eg. 01:60

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,9 @@
 export function formatSecs (secs: number): string {
-  const minsPart = Math.floor(secs / 60).toFixed(0).padStart(2, '0')
-  const secsPart = (secs % 60).toFixed(0).padStart(2, '0')
+  const minsPart = formatPart(secs / 60)
+  const secsPart = formatPart(secs % 60)
   return `${minsPart}:${secsPart}`
 }
 
+function formatPart (n: number): string {
+  return Math.floor(n).toFixed(0).padStart(2, '0')
+}
